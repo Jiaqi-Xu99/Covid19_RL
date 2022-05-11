5ed2b68c68e00a0d05a1bd32bca7b82781326003
JiaqiXu_99 <xu.4015@buckeyemail.osu.edu>
2022-05-11 15:16:47 -0400

    Add modified env file

--- Env/CovidEnvironment.py
+++ Env/CovidEnvironment.py
@@ -5,71 +5,67 @@ import numpy as np
 
 
 class CovidEnv(gym.Env):
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, size=5):
-        self.size = size  # The size of the square grid
-        self.window_size = 512  # The size of the PyGame window
-
-        # Observations are dictionaries with the agent's and the target's location.
-        # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
-        self.observation_space = spaces.Dict(
-            {
-                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-            }
-        )
-
-        # We have 4 actions, corresponding to "right", "up", "left", "down", "right"
-        self.action_space = spaces.Discrete(4)
+    def __init__(self, size):
+        self.size = int(size)  # The size of the second generation
+        self.days = 30  # Assume we observe the 2nd generation for 30 days
+        self.p_symptomatic = 0.8  # probability that an infected person is eventually symptomatic
 
         """
-        The following dictionary maps abstract actions from `self.action_space` to 
-        the direction we will walk in if that action is taken.
-        I.e. 0 corresponds to "right", 1 to "up" etc.
+        Use a dictionary to represent the observation. Each category is represented by a matrix.
+        The row of each matrix represents a 2nd generation case. The column of each matrix represents one day.
         """
-        self._action_to_direction = {
-            0: np.array([1, 0]),
-            1: np.array([0, 1]),
-            2: np.array([-1, 0]),
-            3: np.array([0, -1]),
-        }
+        self.observation_space = spaces.Dict({
+            "Day of exposure": spaces.Box(low=0, high=1, shape=(self.size, self.days), dtype=np.int16),
+            "Any day before exposure": spaces.Box(low=0, high=1, shape=(self.size, self.days), dtype=np.int16),
+            "Showing symptoms": spaces.Box(low=0, high=1, shape=(self.size, self.days), dtype=np.int16),
+            "Not showing symptoms": spaces.Box(low=0, high=1, shape=(self.size, self.days), dtype=np.int16),
+            "Unobserved future": spaces.Box(low=0, high=1, shape=(self.size, self.days), dtype=np.int16)})
 
         """
-        If human-rendering is used, `self.window` will be a reference
-        to the window that we draw to. `self.clock` will be a clock that is used
-        to ensure that the environment is rendered at the correct framerate in
-        human-mode. They will remain `None` until human-mode is used for the
-        first time.
+        We choose to use a discrete number to represent an action space. There should be 2^size scenarios.
+        For example, let assume size = 3,  0 means we don't quarantine any people (think 
+        it as a binary representation 000). 1 means we only quarantine the first person (001).
+        We can infer from this 'rule' that the maximum number of our action 7 (7 = 111) means we quarantine 
+        all three people.
         """
-        self.window = None
-        self.clock = None
+        self.action_space = spaces.Discrete(2 ** self.size)
 
-    def _get_obs(self):
-        return {"agent": self._agent_location, "target": self._target_location}
-
-    def _get_info(self):
-        return {
-            "distance": np.linalg.norm(
-                self._agent_location - self._target_location, ord=1
-            )
-        }
-
+    # We start to trace when the 1st generation case is tested positive.
+    # We assume the 1st generation case is tested positive at day 4.
     def reset(self, seed=None, return_info=False, options=None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
-        # Choose the agent's location uniformly at random
-        self._agent_location = self.np_random.integers(0, self.size, size=2)
+        initial_state = {
+            "Day of exposure": np.zeros((self.size, self.days)),
+            "Any day before exposure": np.zeros((self.size, self.days)),
+            "Showing symptoms": np.zeros((self.size, self.days)),
+            "Not showing symptoms": np.zeros((self.size, self.days)),
+            "Unobserved future": np.zeros((self.size, self.days))}
 
-        # We will sample the target's location randomly until it does not coincide with the agent's location
-        self._target_location = self._agent_location
-        while np.array_equal(self._target_location, self._agent_location):
-            self._target_location = self.np_random.integers(0, self.size, size=2)
+        # Assume every 2nd generation cases are exposed on day 3
+        for i in self.size:
+            initial_state["Day of exposure"][i][2] = 1
 
-        observation = self._get_obs()
-        info = self._get_info()
-        return (observation, info) if return_info else observation
+        for i in self.size:
+            for j in range(2):
+                initial_state["Any day before exposure"][i][j] = 1
+
+        # Assume we haven't found any 2nd generation case have symptoms
+        for i in self.size:
+            for j in range(2):
+                initial_state["Any day before exposure"][i][j] = 1
+
+        # Assume start observing at day 4
+        for i in self.size:
+            for j in range(3, self.days):
+                initial_state["Any day before exposure"][i][j] = 1
+
+        if not return_info:
+            return initial_state
+        else:
+            return initial_state, {}
 
     def step(self, action):
         # Map the action (element of {0,1,2,3}) to the direction we walk in
@@ -86,69 +82,8 @@ class CovidEnv(gym.Env):
 
         return observation, reward, done, info
 
-    def render(self, mode="human"):
-        if self.window is None and mode == "human":
-            pygame.init()
-            pygame.display.init()
-            self.window = pygame.display.set_mode((self.window_size, self.window_size))
-        if self.clock is None and mode == "human":
-            self.clock = pygame.time.Clock()
-
-        canvas = pygame.Surface((self.window_size, self.window_size))
-        canvas.fill((255, 255, 255))
-        pix_square_size = (
-            self.window_size / self.size
-        )  # The size of a single grid square in pixels
-
-        # First we draw the target
-        pygame.draw.rect(
-            canvas,
-            (255, 0, 0),
-            pygame.Rect(
-                pix_square_size * self._target_location,
-                (pix_square_size, pix_square_size),
-            ),
-        )
-        # Now we draw the agent
-        pygame.draw.circle(
-            canvas,
-            (0, 0, 255),
-            (self._agent_location + 0.5) * pix_square_size,
-            pix_square_size / 3,
-        )
-
-        # Finally, add some gridlines
-        for x in range(self.size + 1):
-            pygame.draw.line(
-                canvas,
-                0,
-                (0, pix_square_size * x),
-                (self.window_size, pix_square_size * x),
-                width=3,
-            )
-            pygame.draw.line(
-                canvas,
-                0,
-                (pix_square_size * x, 0),
-                (pix_square_size * x, self.window_size),
-                width=3,
-            )
-
-        if mode == "human":
-            # The following line copies our drawings from `canvas` to the visible window
-            self.window.blit(canvas, canvas.get_rect())
-            pygame.event.pump()
-            pygame.display.update()
-
-            # We need to ensure that human-rendering occurs at the predefined framerate.
-            # The following line will automatically add a delay to keep the framerate stable.
-            self.clock.tick(self.metadata["render_fps"])
-        else:  # rgb_array
-            return np.transpose(
-                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
-            )
+    def render(self):
+        pass
 
     def close(self):
-        if self.window is not None:
-            pygame.display.quit()
-            pygame.quit()
+        pass
